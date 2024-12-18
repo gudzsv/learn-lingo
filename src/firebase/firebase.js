3e0460e1e8bac096d8bd777234fa89546905b161
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-12-18 23:53:46 +0100

    add firebase config to .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { FIREBASE_CONFIG } from '../constants/constants.js';
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyDhQCRSr7AFQp23ddse8jZcHFd_i46CD0s',
-	authDomain: 'learn-lingo-d1fd2.firebaseapp.com',
-	databaseURL:
-		'https://learn-lingo-d1fd2-default-rtdb.europe-west1.firebasedatabase.app/',
-	projectId: 'learn-lingo-d1fd2',
-	storageBucket: 'learn-lingo-d1fd2.firebasestorage.app',
-	messagingSenderId: '714805126462',
-	appId: '1:714805126462:web:767d01dc0e1d30b244386f',
-	measurementId: 'G-66RWH9NDW4',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(FIREBASE_CONFIG);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
